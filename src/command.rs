93ee5eec6cc4ab277fa9ab6c0f101ecbac6d713a
Nordine Bittich <nordine@bittich.be>
2022-01-22 09:10:52 +0100

    read stdout asynchronously

--- src/command.rs
+++ src/command.rs
@@ -1,15 +1,34 @@
-use tokio::io::{self, AsyncWriteExt};
+use std::process::Stdio;
+
+use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 
 use crate::init::ShellCommand;
 
 pub async fn run(command: &ShellCommand, extra_args: &[String]) {
     let args = [&command.get_args()[..], extra_args].concat();
-    let out = Command::new(command.get_command())
-        .args(args)
-        .output()
-        .await
-        .unwrap();
-    let mut stdout = io::stdout();
-    stdout.write_all(&out.stdout[..]).await.unwrap();
+    let mut cmd = Command::new(command.get_command());
+    cmd.args(args);
+
+    cmd.stdout(Stdio::piped());
+
+    let mut child = cmd.spawn().expect("failed to spawn command");
+
+    let stdout = child
+        .stdout
+        .take()
+        .expect("child did not have a handle to stdout");
+
+    let mut reader = BufReader::new(stdout).lines();
+
+    tokio::spawn(async move {
+        let _ = child
+            .wait()
+            .await
+            .expect("child process encountered an error");
+    });
+
+    while let Some(line) = reader.next_line().await.unwrap() {
+        println!("{line}");
+    }
 }
