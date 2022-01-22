665f1830048619e1f820815b10cabcce50e424c5
Nordine Bittich <nordine@bittich.be>
2022-01-22 07:57:51 +0100

    better api

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,23 @@ async fn main() -> io::Result<()> {
     let cli = arguments::Cli::parse();
 
     match &cli.command {
-        arguments::Commands::Info { path } => {
-            println!("'init' was used, name is: {:?}", path);
+        arguments::Commands::Info { path: _ } => {
             let init_file = init::InitFile::get().await;
-            println!("{:?}", init_file);
+            println!("{}", serde_json::to_string_pretty(&init_file).unwrap());
         }
         arguments::Commands::Add { key, command, args } => {
-            println!(
-                "'add was used', key is {:?}, command is {:?}, args is {:?}",
-                key, command, args
-            );
             let mut init_file = init::InitFile::get().await;
             init_file
                 .add(key.to_string(), command.to_string(), args.to_owned())
                 .await?;
         }
         arguments::Commands::Run { key, extra_args } => {
-            println!(
-                "'run' was used, key is: {:?}, extra_args: {:?}",
-                key, extra_args
-            );
             let init_file = init::InitFile::get().await;
             if let Some(sc) = init_file.get_command(key) {
                 xdrc::command::run(sc, extra_args).await;
             }
         }
         arguments::Commands::Delete { key } => {
-            println!("remove was used with key {key}");
             let mut init_file = init::InitFile::get().await;
             println!(
                 "remove shellcommand: {}",
